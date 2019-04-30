37214259a05570b5ba9db2c65d7ec04291ae7bac
omar <omarabdelaziz646@gmail.com>
2019-04-30 01:04:01 -0700

    ddd

--- PMS02/PMS02/Migrations/201904300801297_ha.Designer.cs
+++ PMS02/PMS02/Migrations/201904300801297_ha.Designer.cs
@@ -6,14 +6,14 @@ namespace PMS02.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class EditRespondinSendingRequestTable : IMigrationMetadata
+    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
+    public sealed partial class ha : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(EditRespondinSendingRequestTable));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(ha));
         
         string IMigrationMetadata.Id
         {
-            get { return "201904290348515_EditRespondinSendingRequestTable"; }
+            get { return "201904300801297_ha"; }
         }
         
         string IMigrationMetadata.Source
